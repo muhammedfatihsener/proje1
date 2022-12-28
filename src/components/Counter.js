51b365b65062df431b324ebc3c047bddd986e4a4
Muhammed Fatih Şener <muhammedfatihsener@gmail.com>
2022-12-28 17:14:45 +0300

    formatcounter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,33 +1,32 @@
 import React, { Component } from 'react'
 
- class counter extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            count: 0
-        }
-         }
+class counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
 
-         increment()
-         {
-this.setState({
-    count: this.state.count + 1
-},
-
-
-    () => console.log(this.state.count)
-
-)
+  increment() {
+    this.setState({
+      count: this.state.count + 1
+    },
 
 
+      () => console.log(this.state.count)
 
-         }
+    )
+
+
+
+  }
   render() {
     return (
       <div>
-         <h3>count  {this.state.count}</h3> 
-         <button onClick={() => this.increment()} > artıbuton 1 </button>
-         <button onClick={()=> this.increment() } >esksibuton 2</button> 
+        <h3>count  {this.state.count}</h3>
+        <button onClick={() => this.increment()} > artıbuton 1 </button>
+        <button onClick={() => this.increment()} >esksibuton 2</button>
       </div>
     )
   }
