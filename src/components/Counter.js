5d3ad9ec8143400f912625664199742bdbe3eb14
Muhammed Fatih Şener <muhammedfatihsener@gmail.com>
2022-12-28 17:09:13 +0300

    test

---
+++ src/components/Counter.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+ class counter extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0
+        }
+         }
+
+         increment()
+         {
+this.setState({
+    count: this.state.count + 1
+},
+
+
+    () => console.log(this.state.count)
+
+)
+
+
+
+         }
+  render() {
+    return (
+      <div>
+         <h3>count  {this.state.count}</h3> 
+         <button onClick={() => this.increment()} > artıbuton 1 </button>
+         <button onClick={()=> this.increment() } >esksibuton 2</button> 
+      </div>
+    )
+  }
+}
+
+export default counter;
